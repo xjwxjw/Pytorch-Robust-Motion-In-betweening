b68a39218d3df7094c3891ce2425d5431552ab46
Jingwei Xu <xjwxjw@sjtu.edu.cn>
2020-12-15 11:17:16 +0800

    Bug fix for foot sliding

--- test.py
+++ test.py
@@ -24,17 +24,30 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 from remove_fs import remove_fs, save_bvh_from_network_output
 from foot_sliding.animation_data import y_rotation_from_positions
+from PIL import Image
 
 def plot_pose(pose, cur_frame, prefix):
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
+    parents = [-1, 0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 12, 11, 14, 15, 16, 11, 18, 19, 20]
     ax.cla()
     num_joint = pose.shape[0] // 3
-    ax.scatter(pose[:num_joint, 0], pose[:num_joint, 2], pose[:num_joint, 1], c='r')
-    ax.scatter(pose[num_joint:num_joint*2, 0], pose[num_joint:num_joint*2, 2], pose[num_joint:num_joint*2, 1],c='b')
-    ax.scatter(pose[num_joint*2:num_joint*3, 0], pose[num_joint*2:num_joint*3, 2], pose[num_joint*2:num_joint*3, 1],c='g')
+    for i, p in enumerate(parents):
+        if i > 0:
+            ax.plot([pose[i, 0], pose[p, 0]],\
+                    [pose[i, 2], pose[p, 2]],\
+                    [pose[i, 1], pose[p, 1]], c='r')
+            ax.plot([pose[i+num_joint, 0], pose[p+num_joint, 0]],\
+                    [pose[i+num_joint, 2], pose[p+num_joint, 2]],\
+                    [pose[i+num_joint, 1], pose[p+num_joint, 1]], c='b')
+            ax.plot([pose[i+num_joint*2, 0], pose[p+num_joint*2, 0]],\
+                    [pose[i+num_joint*2, 2], pose[p+num_joint*2, 2]],\
+                    [pose[i+num_joint*2, 1], pose[p+num_joint*2, 1]], c='g')
+    # ax.scatter(pose[:num_joint, 0], pose[:num_joint, 2], pose[:num_joint, 1],c='b')
+    # ax.scatter(pose[num_joint:num_joint*2, 0], pose[num_joint:num_joint*2, 2], pose[num_joint:num_joint*2, 1],c='b')
+    # ax.scatter(pose[num_joint*2:num_joint*3, 0], pose[num_joint*2:num_joint*3, 2], pose[num_joint*2:num_joint*3, 1],c='g')
     xmin = np.min(pose[:, 0])
     ymin = np.min(pose[:, 2])
     zmin = np.min(pose[:, 1])
@@ -68,6 +81,7 @@ if __name__ == '__main__':
                               seq_len = opt['model']['seq_length'], \
                               offset = 40,\
                               train = False, debug=opt['test']['debug'])
+    lafan_data_test.cur_seq_length = opt['model']['seq_length']
     x_mean = lafan_data_test.x_mean.cuda()
     x_std = lafan_data_test.x_std.cuda().view(1, 1, opt['model']['num_joints'], 3)
     lafan_loader_test = DataLoader(lafan_data_test, \
@@ -110,6 +124,8 @@ if __name__ == '__main__':
         loss_total_list = []
         
         for i_batch, sampled_batch in enumerate(lafan_loader_test):
+            # if i_batch != 33:
+            #     continue
             pred_img_list = []
             gt_img_list = []
             img_list = []
@@ -137,7 +153,7 @@ if __name__ == '__main__':
                 root_p = sampled_batch['root_p'].cuda()
                 # X
                 X = sampled_batch['X'].cuda()
-                bs = 12#np.random.choice(X.size(0), 1)[0]
+                bs = 6#np.random.choice(X.size(0), 1)[0]
                 if False:
                     print('local_q:', local_q.size(), \
                         'root_v:', root_v.size(), \
@@ -238,22 +254,22 @@ if __name__ == '__main__':
                     pred_list.append(pos_pred[0])
                     contact_list.append(contact_pred[0])
 
-                    if i_batch < 49:
+                    # if i_batch < 49:
                         # print("pos_pred:", pos_pred.size())
-                        if opt['test']['save_img']:
-                            plot_pose(np.concatenate([X[bs,0].view(22, 3).detach().cpu().numpy(),\
-                                                 pos_pred[0, bs].view(22, 3).detach().cpu().numpy(),\
-                                                 X[bs,-1].view(22, 3).detach().cpu().numpy()], 0),\
-                                                 t, '../results'+version+'/pred')
-                            plot_pose(np.concatenate([X[bs,0].view(22, 3).detach().cpu().numpy(),\
-                                                 X[bs,t+1].view(22, 3).detach().cpu().numpy(),\
-                                                 X[bs,-1].view(22, 3).detach().cpu().numpy()], 0),\
-                                                 t, '../results'+version+'/gt')
-                            pred_img = imageio.imread('../results'+version+'/pred_'+str(t)+'.png')
-                            gt_img = imageio.imread('../results'+version+'/gt_'+str(t)+'.png')
-                            pred_img_list.append(pred_img)
-                            gt_img_list.append(gt_img)
-                            img_list.append(np.concatenate([pred_img, gt_img], 1))
+                    if opt['test']['save_img']:
+                        plot_pose(np.concatenate([X[bs,0].view(22, 3).detach().cpu().numpy(),\
+                                                pos_pred[0, bs].view(22, 3).detach().cpu().numpy(),\
+                                                X[bs,-1].view(22, 3).detach().cpu().numpy()], 0),\
+                                                t, '../results'+version+'/pred')
+                        plot_pose(np.concatenate([X[bs,0].view(22, 3).detach().cpu().numpy(),\
+                                                X[bs,t+1].view(22, 3).detach().cpu().numpy(),\
+                                                X[bs,-1].view(22, 3).detach().cpu().numpy()], 0),\
+                                                t, '../results'+version+'/gt')
+                        pred_img = Image.open('../results'+version+'/pred_'+str(t)+'.png', 'r')
+                        gt_img = Image.open('../results'+version+'/gt_'+str(t)+'.png', 'r')
+                        pred_img_list.append(pred_img)
+                        gt_img_list.append(gt_img)
+                        img_list.append(np.concatenate([pred_img, gt_img.resize(pred_img.size)], 1))
                 
                 
                 # print('pivots:', pivots.shape)
@@ -262,34 +278,48 @@ if __name__ == '__main__':
                     # print("bs:", bs)
                     bvh_data = torch.cat([x[bs].unsqueeze(0) for x in bvh_list], 0).detach().cpu().numpy()
                     # print('bvh_data:', bvh_data.shape)
+                    # print('bvh_data:', bvh_data[0,3:7])
+                    # assert 0
                     write_to_bvhfile(bvh_data, ('../bvh_seq/test_%03d.bvh' % i_batch), opt['data']['joints_to_remove'])
                     # assert 0
                     contact_data = torch.cat([x[bs].unsqueeze(0) for x in contact_list], 0).detach().cpu().numpy()
-                    rot_data = torch.cat([x[bs].unsqueeze(0) for x in quat_list], 0).detach().cpu().numpy()
-                    root_data = torch.cat([x[bs].unsqueeze(0) for x in root_list], 0).detach().cpu().numpy()
-                    root_data = root_data[:,[2,1,0]].copy()
-                    root_data[:,2] *= -1
-                    pred_pose = torch.cat([x[bs].unsqueeze(0) for x in pred_list], 0).detach().cpu().numpy()
-                    quaters, pivots = y_rotation_from_positions(pred_pose, hips = (1,5), sdrs = (14,18))
-                    motion = np.concatenate([rot_data.reshape(rot_data.shape[0], -1),\
-                                             root_data,\
-                                             pivots], -1)
-                    motion = motion.transpose(1,0)
+                    # rot_data = torch.cat([x[bs].unsqueeze(0) for x in quat_list], 0).detach().cpu().numpy()
+                    # root_data = torch.cat([x[bs].unsqueeze(0) for x in root_list], 0).detach().cpu().numpy()
+                    # pred_pose = torch.cat([x[bs].unsqueeze(0) for x in pred_list], 0).detach().cpu().numpy()
+                    # quaters, pivots = y_rotation_from_positions(pred_pose, hips = (1,5), sdrs = (14,18))
+                    # motion = np.concatenate([rot_data.reshape(rot_data.shape[0], -1),\
+                    #                          root_data,\
+                    #                          pivots], -1)
+                    # motion = motion.transpose(1,0)
                     foot = contact_data.transpose(1,0)
                     foot[foot > 0.5] = 1.0
                     foot[foot <= 0.5] = 0.0
                     # print('foot[0]:',foot[0])
-                    remove_fs(motion, \
+                    glb = remove_fs(('../bvh_seq/test_%03d.bvh' % i_batch), \
                               foot, \
                               fid_l=(3, 4), \
                               fid_r=(7, 8),\
-                              output_path=("../bvh_seq"+version+"/test_%03d.bvh" % i_batch))
+                              output_path=("../bvh_seq_after"+version+"/test_%03d.bvh" % i_batch))
+                    fix_img_list = []
+                    for t in range(opt['model']['seq_length']):
+                        plot_pose(np.concatenate([X[bs,0].view(22, 3).detach().cpu().numpy(),\
+                                                glb[t],\
+                                                X[bs,-1].view(22, 3).detach().cpu().numpy()], 0),\
+                                                t, '../results'+version+'/fixed')
+                        plot_pose(np.concatenate([X[bs,0].view(22, 3).detach().cpu().numpy(),\
+                                                X[bs,t].view(22, 3).detach().cpu().numpy(),\
+                                                X[bs,-1].view(22, 3).detach().cpu().numpy()], 0),\
+                                                t, '../results'+version+'/gt')
+                        fix_img = Image.open('../results'+version+'/fixed_'+str(t)+'.png', 'r')
+                        gt_img = Image.open('../results'+version+'/gt_'+str(t)+'.png', 'r')
+                        fix_img_list.append(np.concatenate([fix_img, gt_img.resize(fix_img.size)], 1))
+                    imageio.mimsave(('../gif'+version+'/img_fix_%03d.gif' % i_batch), fix_img_list, duration=0.1)
                     # save_bvh_from_network_output(motion, output_path=("../bvh_seq_after/test_%03d.bvh" % i_batch))
                     
 
-                if i_batch < 49:
-                    if opt['test']['save_img'] and opt['test']['save_gif']:
-                        imageio.mimsave(('../gif'+version+'/img_%03d.gif' % i_batch), img_list, duration=0.1)
+                # if i_batch < 49:
+                if opt['test']['save_img'] and opt['test']['save_gif']:
+                    imageio.mimsave(('../gif'+version+'/img_%03d.gif' % i_batch), img_list, duration=0.1)
                 if opt['test']['save_pose']:
                     gt_pose = X[bs,:].view(opt['model']['seq_length'], 22, 3).detach().cpu().numpy()
                     pred_pose = torch.cat([x[bs].unsqueeze(0) for x in pred_list], 0).detach().cpu().numpy()
@@ -301,8 +331,8 @@ if __name__ == '__main__':
                     plt.savefig('../results'+version+'/pose_%03d.png' % i_batch)
                     plt.close()
 
-                if opt['test']['save_img'] and i_batch > 49:
-                    break
+                # if opt['test']['save_img'] and i_batch > 49:
+                #     break
 
                 if opt['test']['save_pose'] and i_batch > 49:
                     break
